7b81a807fe82445960bef6b64a0fab6d1830aa2a
Kitae Kim <kitae@thirdaxis.io>
2024-05-17 11:38:37 -0400

    updated height and radius

--- src/environments.js
+++ src/environments.js
@@ -1,7 +1,8 @@
 export let environments = {
     0:{
         name: "Overcast Sky",
-
+        height: 10,
+        radius: 1000,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/skyovercast_kloofendal_overcast_puresky_2k.jpg",
@@ -14,7 +15,8 @@ export let environments = {
     },
     1:{
         name: "Airfield Day",
-        
+        height: 0.10,
+        radius: 1000,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/airfieldday_cgib_RAY_062127_HDR_02_11542980-RES_2K.jpg",
@@ -28,7 +30,8 @@ export let environments = {
     },
     2:{
         name: "Architecture Night",
-        
+        height: 1.5,
+        radius: 13.71,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/architecturenight_cgib_XIE_062222_HDR_02_11548109_RAY-RES_2K.jpg",
@@ -42,7 +45,8 @@ export let environments = {
     },
     3:{
         name: "Bay Day",
-
+        height: 5,
+        radius: 400,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/bayday_cgib_XIE_062229_HDR_01_11549206_RAY-RES_2K.jpg",
@@ -56,8 +60,8 @@ export let environments = {
     },
     4:{
         name: "Beach Day",
-        height: 55,
-        radius: 400,
+        height: 4.13,
+        radius: 78.16,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/beachday_cgib_TOR_061912_HDR_07_11529106_RAY-RES_2K.jpg",
@@ -70,7 +74,8 @@ export let environments = {
     },
     5:{
         name: "Courtyard Day",
-
+        height: 2,
+        radius: 16.71,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/courtyardday_cgib_XIE_062023_HDR_03_11537082-RES_2K.jpg",
@@ -83,7 +88,8 @@ export let environments = {
     },
     6:{
         name: "City Night",
-
+        height: 1.67,
+        radius: 29,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/citynight_cgib_XIE_062053_HDR_01_11539060_RAY-RES_2K.jpg",
@@ -97,8 +103,8 @@ export let environments = {
     },
     7:{
         name: "Canyon Day",
-        height:32,
-        radius:115,
+        height: 1,
+        radius: 10,
         category:"World",
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/canyonday_cgib_ASH_059296_HDR_02_11422616_RAY-RES_2K.jpg",
         hdrjpg4k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/canyonday_cgib_ASH_059296_HDR_02_11422616_RAY-RES_4K.jpg",
@@ -107,7 +113,8 @@ export let environments = {
     },
     8:{
         name: "Desert Dusk",
-
+        height: 1,
+        radius: 16.71,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/desertdusk_cgib_HAH_062227_HDR_04_11549067_RAY-RES_2K.jpg",
@@ -121,8 +128,9 @@ export let environments = {
     },
     9:{
         name: "Forest Day",
-        
-        category:"World",
+        height: 1,
+        radius: 3,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/forestday_cgib_BCH_061368_HDR_01_11500186_RAY-RES_2K.jpg",
 
@@ -135,8 +143,9 @@ export let environments = {
     },
     10:{
         name: "Loft Day",
-        
-        category:"World",
+        height: 0.10,
+        radius: 1000,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/loftday_cgib_PAR_055726_HDR_01_11282519_RAY-RES_2K.jpg",
 
@@ -149,7 +158,8 @@ export let environments = {
     },
     11:{
         name: "Ocean Sunset",
-
+        height: 9,
+        radius: 1000,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/belfast_sunset_puresky_2k.jpg",
@@ -163,8 +173,9 @@ export let environments = {
     },
     12:{
         name: "Garage Day",
-
-        category:"World",
+        height: 2.5,
+        radius: 6.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/garageday_cgib_BCH_061902_HDR_02_11528224_RAY-RES_2K.jpg",
 
@@ -176,7 +187,8 @@ export let environments = {
     },
     13:{
         name: "Pools Day",
-
+        height: 1.67,
+        radius: 176.49,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/poolsday_cgib_ONT_054505_HDR_01_11257328_RAY-RES_2K.jpg",
@@ -190,7 +202,8 @@ export let environments = {
     },
     14:{
         name: "Architecture Dusk",
-
+        height: 1,
+        radius: 6.42,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/architecturedusk_cgib_ING_058224_HDR_04_11380761-RES_2K.jpg",
@@ -204,8 +217,9 @@ export let environments = {
     },
     15:{
         name: "Winter Day",
-
-        category:"World",
+        height: 1.5,
+        radius: 16.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/winterday_cgib_BCH_062131_HDR_01_11543110_RAY-RES_2K.jpg",
 
@@ -219,8 +233,9 @@ export let environments = {
 
     16:{
         name: "Tunnel Bright",
-
-        category:"World",
+        height: 1.5,
+        radius: 3.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/tunnelbright_cgib_JMA_046530_HDR_01_11143245-RES_2K.jpg",
 
@@ -233,7 +248,8 @@ export let environments = {
     },
     17:{
         name: "Factory Dark",
-
+        height: 1.67,
+        radius: 9,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/factorydark_cgib_VER_047369_HDR_02_11147199-RES_2K.jpg",
@@ -248,7 +264,8 @@ export let environments = {
 
     18:{
         name: "Sky Bright",
-
+        height: 6.59,
+        radius: 1000,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/seamorning_kloofendal_48d_partly_cloudy_puresky_2k.jpg",
@@ -263,8 +280,9 @@ export let environments = {
 
     19:{
         name: "Architecture Colorful",
-
-        category:"World",
+        height: 1.67,
+        radius: 23.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/architecturerainbow_cgib_TOR_060380_HDR_01_11456021-RES_2K.jpg",
 
@@ -278,8 +296,9 @@ export let environments = {
 
     20:{
         name: "Architecture Rainy",
-
-        category:"World",
+        height: 1.44,
+        radius: 6.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/architecturerainy_cgib_XIE_062044_HDR_04_11538307_RAY-RES_2K.jpg",
         
@@ -293,8 +312,9 @@ export let environments = {
     
     21:{
         name: "Tokyo Night",
-    
-        category:"World",
+        height: 1.5,
+        radius: 3.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/tokyonight_cgib_JPN_062480_HDR_04_11568864_RAY-RES_2K.jpg",
         
@@ -308,8 +328,9 @@ export let environments = {
 
     22:{
         name: "Greathall Bright",
-       
-        category:"World",
+        height: 1,
+        radius: 15,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/greathallbright_cgib_RKE_055302_HDR_01_11271460-RES_2K.jpg",
 
@@ -323,8 +344,9 @@ export let environments = {
 
     23:{
         name: "Interior Bright",
-       
-        category:"World",
+        height: 1,
+        radius: 3.42,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/interiorbright_cgib_BCH_052386_HDR_01_11201760_RAY-RES_2K.jpg",
 
@@ -338,8 +360,9 @@ export let environments = {
 
     24:{
         name: "Hangar Bright",
-
-        category:"World",
+        height: 1,
+        radius: 10,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/hangarbright_cgib_DAO_057516_HDR_04_11338666-RES_2K.jpg",
 
@@ -353,8 +376,9 @@ export let environments = {
 
     25:{
         name: "Lobby Night",
-
-        category:"World",
+        height: 1,
+        radius: 5,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/lobbynight_cgib_JPN_062308_HDR_01_11555397_RAY-RES_2K.jpg",
 
@@ -367,7 +391,8 @@ export let environments = {
 
     26:{
         name: "Studio Bright",
-
+        height: 1,
+        radius: 2,
         category:"Studio",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/photostudio_small_09_2k.jpg",
@@ -381,8 +406,9 @@ export let environments = {
 
     27:{
         name: "Studio Dark",
-
-        category:"Studio",
+        height: 1.5,
+        radius: 3,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/photostudioTomoco_2K.jpg",
 
@@ -395,8 +421,9 @@ export let environments = {
 
     28:{
         name: "Station Afternoon",
-
-        category:"World",
+        height: 2,
+        radius: 6.58,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/stationafternoon_cgib_BCH_057707_HDR_01_11348728-RES_2K.jpg",
 
@@ -410,8 +437,9 @@ export let environments = {
 
     29:{
         name: "Urban Dark",
-
-        category:"World",
+        height: 1.67,
+        radius: 4.42,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/urbandark_cgib_USC_062322_HDR_05_11556172_RAY-RES_2K.jpg",
 
@@ -424,8 +452,9 @@ export let environments = {
 
     30:{
         name: "Architecture Lights",
-
-        category:"World",
+        height: 1.67,
+        radius: 5.42,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/architecturelights_cgib_FHA_056143_HDR_03_11294448-RES_2K.jpg",
 
@@ -438,8 +467,9 @@ export let environments = {
     
     31:{
         name: "NYC Night",
-
-        category:"World",
+        height: 2,
+        radius: 16.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/nycnight_cgib_DAO_056578_HDR_01_11308248-RES_2K.jpg",
 
@@ -452,8 +482,9 @@ export let environments = {
     
     32:{
         name: "NYC Overcast",
-
-        category:"World",
+        height: 2,
+        radius: 29,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/nycovercast_cgib_EMC_059157_HDR_01_11416580-RES_2K.jpg",
 
@@ -465,8 +496,9 @@ export let environments = {
     },
     33:{
         name: "Oculus Day",
-
-        category:"World",
+        height: 1,
+        radius: 4,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/oculusday_cgib_EMC_059158_HDR_01_11416605_RAY-RES_2K.jpg",
 
@@ -479,8 +511,9 @@ export let environments = {
     
     34:{
         name: "Oculus Night",
-
-        category:"World",
+        height: 2,
+        radius: 8.71,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/oculusnight_cgib_DAO_056579_HDR_01_11308261-RES_2K.jpg",
 
@@ -493,8 +526,9 @@ export let environments = {
     
     35:{
         name: "Paris Morning",
-
-        category:"World",
+        height: 2,
+        radius: 8,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/parismorning_cgib_ING_058275_HDR_02_11383132-RES_2K.jpg",
 
@@ -507,8 +541,9 @@ export let environments = {
 
     36:{
         name: "Paris Afternoon",
-
-        category:"World",
+        height: 2,
+        radius: 10,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/parisafternoon_cgib_ING_058278_HDR_02_11383340-RES_2K.jpg",
 
@@ -521,7 +556,8 @@ export let environments = {
 
     37:{
         name: "Paris Overcast",
-
+        height: 1.67,
+        radius: 8.71,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/parisovercast_cgib_ING_058274_HDR_07_11383065-RES_2K.jpg",
@@ -535,8 +571,9 @@ export let environments = {
 
     38:{
         name: "Tokyo Day",
-
-        category:"World",
+        height: 2,
+        radius: 7,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/tokyoday_cgib_JPN_062480_HDR_02_11568862_RAY-RES_2K.jpg",
 
@@ -548,8 +585,9 @@ export let environments = {
     },
     39:{
         name: "Entry Warm",
-
-        category:"World",
+        height: 2,
+        radius: 5,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/entrancewarm_cgib_JPN_062280_HDR_06_11553555_RAY-RES_2K.jpg",
 
@@ -561,8 +599,9 @@ export let environments = {
     },
     40:{
         name: "Overpass Dark",
-
-        category:"World",
+        height: 1.5,
+        radius: 12,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/overpassovercast_cgib_EMC_059155_HDR_01_11416523-RES_2K.jpg",
 
@@ -574,7 +613,8 @@ export let environments = {
     },
     41:{
         name: "Iceland Morning",
-
+        height: 1,
+        radius: 3,
         category:"World",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/icelandmorning_cgib_JMA_021006_HDR_01_11057860-RES_2K.jpg",
@@ -588,7 +628,8 @@ export let environments = {
 
     42:{
         name: "Substance Studio Tomoco",
-
+        height: 0.10,
+        radius: 1000,
         category:"Studio",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/substance_StudioTomoco_2K.jpg",
@@ -601,8 +642,9 @@ export let environments = {
     },
     43:{
         name: "Substance Studio 6",
-
-        category:"Studio",
+        height: 1,
+        radius: 2,
+        category:"Box",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/substance_Studio_06_2K.jpg",
 
@@ -614,7 +656,8 @@ export let environments = {
     },
     44:{
         name: "Modelviewer Studio Neutral",
-
+        height: 2,
+        radius: 3,
         category:"Studio",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/modelviewer_neutral.jpg",
@@ -628,7 +671,8 @@ export let environments = {
 
     45:{
         name: "Rhino Studio",
-
+        height: 1,
+        radius: 2,
         category:"Studio",
 
         hdrjpg2k:"https://foveate3dn.nyc3.cdn.digitaloceanspaces.com/foveate_site_prod_assets/hdrs/Rhino_Studio_2K.jpg",
